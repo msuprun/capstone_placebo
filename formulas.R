b1fce65c50d55705c261eaf47be2f9e1e6eb787c
maria suprun <suprun.maria@gmail.com>
2014-03-26 19:21:38 -0400

    Cleaned up and updated Responder Rate and Effect Size

--- formulas.R
+++ formulas.R
@@ -1,7 +1,4 @@
-rm(list=ls())
-
-baselineDiary <- baseline(nSubjects=200, nDays=28, nMonths=2)
-maintDiary <- maint(nSubjects=200, nDays=28, nMonths=4)
+# Outcome measure calculations for epilpsy trials 
 
 #################################################################################
 ########################### EFFECT SIZE #########################################
@@ -11,6 +8,7 @@ maintDiary <- maint(nSubjects=200, nDays=28, nMonths=4)
 # /(#of seiz/28d at baseline).
 # average across subjects = effect size 
 # only for placebo group
+
 nSeizBase46Days <- rowSums(baselineDiary[which(baselineDiary$group==0),4:59])
 nSeizMaint112Days <- rowSums(maintDiary[which(baselineDiary$group==0),3:114])
 
@@ -21,12 +19,12 @@ effectSizePlacebo <- mean(seizureReducGr0)
 #################################################################################
 ########################### RESPONDER RATE ######################################
 #################################################################################
+# Responder - a patient with >= 50% reduction in monthly # of seizures.
+# Outcome is # of responders in the study out of the total sample size 
+# Responder Rate: # of responders to placebo (V151) / total N in placebo = proportion
 
-responder <- ifelse(seizureReducGr0 >= 0.5*(28*(nSeizBase46Days/56)), 1,0)
-
-
-sum(baselineDiary[2, 4:59])
-sum(maintDiary[2, 3:114])
-0.5*((40/56)*28) > (37/112)*28
-
+responder <- ifelse((nSeizMaint112Days/112)*28 < 0.5*(28*(nSeizBase46Days/56)), 1,0)
+responderRate <- 100*(sum(responder)/
+                        nrow(baselineDiary[which(baselineDiary$group==0),]))
+  
 
